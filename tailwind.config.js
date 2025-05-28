54d56d7a799a8e7ada05066d775070a886a6d831
Abba710 <Abbasaifer3@gmail.com>
2025-05-28 18:53:19 +0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+// tailwind.config.js
+module.exports = {
+  theme: {
+    extend: {
+      fontFamily: {
+        raleway: ["Raleway", "sans-serif"],
+        worksans: ["Work Sans", "sans-serif"],
+      },
+    },
+  },
+  content: ["./src/**/*.{html,js}"],
+};
